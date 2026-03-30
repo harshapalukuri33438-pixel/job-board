4427216107d694be2005270bae4c60d96e89b04c
HARSHA VARDAN <harshapalukuri33438@gmail.com>
2026-03-30 18:29:55 +0530

    update changes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -41,7 +41,7 @@ router.post('/login', async (req, res) => {
 
     const token = jwt.sign(
       { id: user._id , role: user.role },
-      'secretkey',
+       process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
 
